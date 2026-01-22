ddb47390887b89de23c39b61c4fe2ca57764048b
abc <abc@gmail.com>
2026-01-22 11:24:00 -0800

    operator

---
+++ Data Types/logical operator.py
@@ -0,0 +1,30 @@
+print("5 == 5:", 5 == 5)
+print("5 == 6:", 5 == 6)
+
+# Not equal to
+print("5 != 6", 5 != 6)
+
+# Greater than
+print("7 > 3:", 7 > 3)
+
+# Less than
+print("2 < 8:", 2 < 8)
+
+# Greater than or equal to
+print("5 >= 5:", 5 >= 5)
+
+# Less than or equal to
+print("3 <= 2:", 3 <= 2)
+
+# Logical Operators
+# and operator
+print("True and True:", True and True)
+print("True and False:", True and False)
+
+# or operator
+print("True or False:", True or False)
+print("Fasle or False:", False or False)
+
+# not operator
+print("not True:", not True)
+print("not Fasle:", not False)
