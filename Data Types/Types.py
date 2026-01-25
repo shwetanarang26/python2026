a7ecd15f3e08ade9a8d38123c483ac9ebd4bc99d
abc <abc@gmail.com>
2026-01-25 15:25:08 -0800

    Types

---
+++ Data Types/Types.py
@@ -0,0 +1,48 @@
+x = 100
+y = "Python"
+z = 3.14
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+first_name = "Dhaanvi"
+greeting = "Happy Birthday " + first_name
+
+print(greeting)
+
+
+msg = "Tacos"
+
+new_msg = "My favorite food " + "is " + msg
+print(new_msg)
+
+#print("Enter a small int:")
+#small_int = input()
+
+#print("The small int is " + small_int )
+
+#large_int = input("Enter a number: ")
+#print("The large number is " + large_int)
+
+name = "Sham"
+print("Hello to", name, "who is from city", 7)
+
+print("It's time to go")
+print('I said "I will come today" ')
+
+print("save" .islower())
+
+print("ms. brown".title())
+
+fav_color = input("Whats your favorite color?: ")
+print(fav_color.capitalize())
+
+data = [6, 7, 5, ["yellow", 78], 67]
+print(data)
+print( data[3] )
+
+print( data[3][1] )
+data[1] = 9
+print(data)
